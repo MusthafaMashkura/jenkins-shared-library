b345c8ba476cdc0acb0f62370da0d0ce4fff2648
MusthafaMashkura <47938530+MusthafaMashkura@users.noreply.github.com>
2023-01-19 19:25:42 +0530

    Update sonarqube.groovy

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -2,6 +2,6 @@ def sonarAnalysis(String Project_Key, String Project_Name){
       sh "mvn sonar:sonar \
       -Dsonar.projectName=${Project_Name} \
       -Dsonar.projectKey=${Project_Key}"
- //     -Dsonar.host.url=${Sonar_URL}  \
+ //     -Dsonar.host.url=${Sonar_URL}  \   //url and login details will be taken from the sonarqube server plugin
  //     -Dsonar.login=${Sonar_login}"
 }
