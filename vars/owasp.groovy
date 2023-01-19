6ec3a2138893a15ff761ac96e152f98afb75fd0a
MusthafaMashkura <47938530+MusthafaMashkura@users.noreply.github.com>
2023-01-19 20:56:22 +0530

    Update owasp.groovy

--- vars/owasp.groovy
+++ vars/owasp.groovy
@@ -3,7 +3,6 @@ def dast() {
                 docker pull owasp/zap2docker-stable
                 docker run -dt --name owasp owasp/zap2docker-stable sh
                 docker exec owasp mkdir /zap/wrk
- //               docker exec owasp zap-baseline.py -t "${appurl}" -x report.xml -I
                 docker exec owasp zap-baseline.py -t http://13.232.140.244:8080/hello-world-maven/ -x report.xml -I
                 echo $WORKSPACE
                 docker cp owasp:/zap/wrk/report.xml $WORKSPACE/report.xml
