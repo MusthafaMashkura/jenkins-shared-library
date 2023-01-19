ee65bdb5e381159ad7ab1e67f821a9cb7c14ea92
MusthafaMashkura <47938530+MusthafaMashkura@users.noreply.github.com>
2023-01-19 14:31:20 +0530

    Update sourcecode.groovy

--- vars/sourcecode.groovy
+++ vars/sourcecode.groovy
@@ -1,4 +1,4 @@
 def git(String branch, String repourl) {
 //  git branch: 'main', url: 'https://github.com/MusthafaMashkura/jenkins-cicd-ansible.git'
-  git branch: ${branch}, url: ${repourl}
+  git branch: "${branch}", url: "${repourl}"
 }
