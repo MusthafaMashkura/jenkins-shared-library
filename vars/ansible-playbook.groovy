a910b2c8f630da943f5381778ec3d72aeaad967e
MusthafaMashkura <47938530+MusthafaMashkura@users.noreply.github.com>
2023-01-19 20:02:37 +0530

    Update ansible-playbook.groovy

--- vars/ansible-playbook.groovy
+++ vars/ansible-playbook.groovy
@@ -1,3 +1,3 @@
 def playbook(String inventoryfile, String playbookfile) {
-sh "ansible-playbook --vault-password-file=/var/lib/jenkins/.vault_pass -i "$inventoryfile" "$playbookfile""
+sh "ansible-playbook --vault-password-file=/var/lib/jenkins/.vault_pass -i $inventoryfile $playbookfile"
 }
